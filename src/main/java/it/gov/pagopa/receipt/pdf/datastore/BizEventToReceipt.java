7b999fe1599ad44a6b43463ad7342f858cc30ddc
svariant <samuele.varianti@nttdata.com>
2023-06-08 18:08:16 +0200

    [PRDP-17] Changed QueueClient to annotation @QueueOutput

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceipt.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceipt.java
@@ -6,14 +6,11 @@ import com.azure.storage.queue.QueueClientBuilder;
 import com.azure.storage.queue.models.SendMessageResult;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
-import com.microsoft.azure.functions.annotation.CosmosDBOutput;
-import com.microsoft.azure.functions.annotation.CosmosDBTrigger;
-import com.microsoft.azure.functions.annotation.ExponentialBackoffRetry;
-import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.*;
 import it.gov.pagopa.receipt.pdf.datastore.entities.event.BizEvent;
 import it.gov.pagopa.receipt.pdf.datastore.entities.event.enumeration.BizEventStatusType;
-import it.gov.pagopa.receipt.pdf.datastore.entities.event.enumeration.ReasonErrorCode;
-import it.gov.pagopa.receipt.pdf.datastore.entities.event.enumeration.ReceiptStatusType;
+import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReasonErrorCode;
+import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReceiptStatusType;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.EventData;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.ReasonError;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.Receipt;
@@ -27,10 +24,6 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class BizEventToReceipt {
-
-	private final String cosmosReceiptQueueConnString = System.getenv("COSMOS_RECEIPT_QUEUE_CONN_STRING");
-	private final String cosmosReceiptQueueTopic = System.getenv("COSMOS_RECEIPT_QUEUE_TOPIC");
-
 
 	@FunctionName("BizEventToReceiptProcessor")
 	@ExponentialBackoffRetry(maxRetryCount = 5, minimumInterval = "500", maximumInterval = "5000")
@@ -50,15 +43,16 @@ public class BizEventToReceipt {
 					collectionName = "receipts",
 					connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
 			OutputBinding<List<Receipt>> documentdb,
+			@QueueOutput(
+					name = "QueueReceiptWaitingForGen",
+					queueName = "COSMOS_RECEIPT_QUEUE_TOPIC",
+					connection = "COSMOS_RECEIPT_QUEUE_CONN_STRING")
+			OutputBinding<String> queueMessage,
 			final ExecutionContext context) {
 		
 		List<Receipt> itemsDone = new ArrayList<>();
 		List<BizEvent> itemsToNotify = new ArrayList<>();
 		Logger logger = context.getLogger();
-		QueueClient queueClient = new QueueClientBuilder()
-				.connectionString(cosmosReceiptQueueConnString)
-				.queueName(cosmosReceiptQueueTopic)
-				.buildClient();
 
 		String msg = String.format("BizEventEnrichment stat %s function - num events triggered %d", context.getInvocationId(),  items.size());
 		logger.info(msg);
@@ -88,25 +82,16 @@ public class BizEventToReceipt {
 					String messageText = ObjectMapperUtils.writeValueAsString(be);
 
 					// Add a message to the queue
-					Response<SendMessageResult> sendMessageResult = queueClient.sendMessageWithResponse(
-							messageText, Duration.of(30, ChronoUnit.SECONDS),
-							null, null, null);
+					queueMessage.setValue(messageText);
 
-					if (sendMessageResult.getStatusCode() != 200) {
-						receipt.setStatus(ReceiptStatusType.NOT_QUEUE_SENT);
-
-						ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_QUEUE.getCode(), "Error sending message to queue");
-						receipt.setReasonErr(reasonError);
-					} else {
-						receipt.setStatus(ReceiptStatusType.INSERTED);
-					}
+					receipt.setStatus(ReceiptStatusType.INSERTED);
 
 				} catch (Exception e) {
 					receipt.setStatus(ReceiptStatusType.NOT_QUEUE_SENT);
-					ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_QUEUE.getCode(), "Generic exception error: "+e.getMessage());
+					ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_QUEUE.getCode(), "Error sending message to queue: "+e.getMessage());
 					receipt.setReasonErr(reasonError);
 
-					logger.severe("Generic exception sending msg to queue at "+ LocalDateTime.now()+ " : " + e.getMessage());
+					logger.severe("Error sending msg to queue at "+ LocalDateTime.now()+ " : " + e.getMessage());
 				}
 
 				itemsDone.add(receipt);
