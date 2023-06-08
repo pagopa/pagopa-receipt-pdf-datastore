23d304e317657c956bea68a61de58f94e979383f
svariant <samuele.varianti@nttdata.com>
2023-06-08 18:08:58 +0200

    [PRDP-17] Completed unit test for successful run

--- src/test/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceiptTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceiptTest.java
@@ -1,55 +1,88 @@
 package it.gov.pagopa.receipt.pdf.datastore;
 
-import com.microsoft.azure.functions.*;
-import it.gov.pagopa.receipt.pdf.datastore.entities.event.BizEvent;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.OutputBinding;
+import it.gov.pagopa.receipt.pdf.datastore.entities.event.*;
+import it.gov.pagopa.receipt.pdf.datastore.entities.event.enumeration.BizEventStatusType;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.Receipt;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BizEventToReceiptTest {
 
+    private final String PAYER_FISCAL_CODE = "a valid payer CF";
+    private final String DEBTOR_FISCAL_CODE = "a valid debtor CF";
+    private final String EVENT_ID = "a valid id";
+
     @Spy
-    BizEventToReceipt function;
+    private BizEventToReceipt function;
 
     @Mock
-    ExecutionContext context;
+    private ExecutionContext context;
+
+    @Captor
+    private ArgumentCaptor<List<Receipt>> captor;
+
+    private BizEvent generateValidBizEvent(){
+        BizEvent item = new BizEvent();
+
+        Payer payer = new Payer();
+        payer.setEntityUniqueIdentifierValue(PAYER_FISCAL_CODE);
+        Debtor debtor = new Debtor();
+        debtor.setEntityUniqueIdentifierValue(DEBTOR_FISCAL_CODE);
+
+        TransactionDetails transactionDetails = new TransactionDetails();
+        Transaction transaction = new Transaction();
+        transaction.setCreationDate(String.valueOf(Instant.now()));
+        transactionDetails.setTransaction(transaction);
+
+        item.setEventStatus(BizEventStatusType.DONE);
+        item.setId(EVENT_ID);
+        item.setPayer(payer);
+        item.setDebtor(debtor);
+        item.setTransactionDetails(transactionDetails);
+
+        return item;
+    }
 
     @Test
     void runOk() {
         // test precondition
-        Logger logger = Logger.getLogger("example-test-logger");
+        Logger logger = Logger.getLogger("BizEventToReceipt-test-logger");
         when(context.getLogger()).thenReturn(logger);
 
-        final HttpResponseMessage.Builder builder = mock(HttpResponseMessage.Builder.class);
+        List<BizEvent> bizEventItems = new ArrayList<>();
+        bizEventItems.add(generateValidBizEvent());
 
-        BizEvent item = mock(BizEvent.class);
-        List<BizEvent> items = new ArrayList<BizEvent>(Arrays.asList(item));
-
-        Receipt receipt = mock(Receipt.class);
-        List<Receipt> listReceipt = new ArrayList<Receipt>(Arrays.asList(receipt));
-        //OutputBinding<List<Receipt>> documentdb = new OutputBinding<List<Receipt>>();
-
-        HttpResponseMessage responseMock = mock(HttpResponseMessage.class);
-        doReturn(HttpStatus.OK).when(responseMock).getStatus();
-        doReturn(responseMock).when(builder).build();
+        @SuppressWarnings("unchecked")
+        OutputBinding<List<Receipt>> documentdb = (OutputBinding<List<Receipt>>) spy(OutputBinding.class);
+        @SuppressWarnings("uncheked")
+        OutputBinding<String> queueMessage = (OutputBinding<String>) mock(OutputBinding.class);
 
         // test execution
-        //HttpResponseMessage response = function.processBizEventEnrichment(items, OutputBinding<List<Receipt>> documentdb);
+        assertDoesNotThrow(() -> function.processBizEventToReceipt(bizEventItems, documentdb, queueMessage, context));
 
-        // test assertion
-        //assertEquals(HttpStatus.OK, response.getStatus());
+        List<Receipt> argumentList = new ArrayList<>();
+
+        verify(documentdb).setValue(captor.capture());
+        Receipt captured = captor.getValue().get(0);
+        assertEquals(EVENT_ID, captured.getIdEvent());
+        assertEquals(PAYER_FISCAL_CODE, captured.getEventData().getPayerFiscalCode());
+        assertEquals(DEBTOR_FISCAL_CODE, captured.getEventData().getDebtorFiscalCode());
     }
 }
