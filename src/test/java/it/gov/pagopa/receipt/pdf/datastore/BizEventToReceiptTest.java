2601b037f4db95bda96a10b218e44ff59ea754c1
svariant <samuele.varianti@nttdata.com>
2023-06-09 12:38:28 +0200

    [PRDP-17] Completed unit tests for BizEventToReceipt function - defined QueueService class for sending messages to queue

--- src/test/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceiptTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceiptTest.java
@@ -1,19 +1,23 @@
 package it.gov.pagopa.receipt.pdf.datastore;
 
+import com.azure.core.http.rest.Response;
+import com.azure.storage.queue.models.SendMessageResult;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
 import it.gov.pagopa.receipt.pdf.datastore.entities.event.*;
 import it.gov.pagopa.receipt.pdf.datastore.entities.event.enumeration.BizEventStatusType;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.Receipt;
+import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReasonErrorCode;
+import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReceiptStatusType;
+import it.gov.pagopa.receipt.pdf.datastore.service.QueueService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -29,14 +33,16 @@ class BizEventToReceiptTest {
     private final String DEBTOR_FISCAL_CODE = "a valid debtor CF";
     private final String EVENT_ID = "a valid id";
 
-    @Spy
     private BizEventToReceipt function;
 
     @Mock
     private ExecutionContext context;
 
     @Captor
-    private ArgumentCaptor<List<Receipt>> captor;
+    private ArgumentCaptor<List<Receipt>> receiptCaptor;
+
+    @Captor
+    private ArgumentCaptor<BizEvent> messageCaptor;
 
     private BizEvent generateValidBizEvent(){
         BizEvent item = new BizEvent();
@@ -48,7 +54,7 @@ class BizEventToReceiptTest {
 
         TransactionDetails transactionDetails = new TransactionDetails();
         Transaction transaction = new Transaction();
-        transaction.setCreationDate(String.valueOf(Instant.now()));
+        transaction.setCreationDate(String.valueOf(LocalDateTime.now()));
         transactionDetails.setTransaction(transaction);
 
         item.setEventStatus(BizEventStatusType.DONE);
@@ -62,25 +68,58 @@ class BizEventToReceiptTest {
 
     @Test
     void runOk() {
-        // test precondition
         Logger logger = Logger.getLogger("BizEventToReceipt-test-logger");
         when(context.getLogger()).thenReturn(logger);
+
+        QueueService serviceMock = mock(QueueService.class);
+        Response<SendMessageResult> response = mock(Response.class);
+        when(response.getStatusCode()).thenReturn(200);
+        when(serviceMock.sendMessageToQueue(anyString())).thenReturn(response);
+
+        function = new BizEventToReceipt(serviceMock);
 
         List<BizEvent> bizEventItems = new ArrayList<>();
         bizEventItems.add(generateValidBizEvent());
 
         @SuppressWarnings("unchecked")
         OutputBinding<List<Receipt>> documentdb = (OutputBinding<List<Receipt>>) spy(OutputBinding.class);
-        @SuppressWarnings("uncheked")
-        OutputBinding<String> queueMessage = (OutputBinding<String>) mock(OutputBinding.class);
 
         // test execution
-        assertDoesNotThrow(() -> function.processBizEventToReceipt(bizEventItems, documentdb, queueMessage, context));
+        assertDoesNotThrow(() -> function.processBizEventToReceipt(bizEventItems, documentdb, context));
 
-        List<Receipt> argumentList = new ArrayList<>();
+        verify(documentdb).setValue(receiptCaptor.capture());
+        Receipt captured = receiptCaptor.getValue().get(0);
+        assertEquals(ReceiptStatusType.INSERTED, captured.getStatus());
+        assertEquals(EVENT_ID, captured.getIdEvent());
+        assertEquals(PAYER_FISCAL_CODE, captured.getEventData().getPayerFiscalCode());
+        assertEquals(DEBTOR_FISCAL_CODE, captured.getEventData().getDebtorFiscalCode());
+    }
 
-        verify(documentdb).setValue(captor.capture());
-        Receipt captured = captor.getValue().get(0);
+    @Test
+    void errorAddingMessageToQueue() {
+        Logger logger = Logger.getLogger("BizEventToReceipt-test-logger");
+        when(context.getLogger()).thenReturn(logger);
+
+        QueueService serviceMock = mock(QueueService.class);
+        Response<SendMessageResult> response = mock(Response.class);
+        when(response.getStatusCode()).thenReturn(400);
+        when(serviceMock.sendMessageToQueue(anyString())).thenReturn(response);
+
+        function = new BizEventToReceipt(serviceMock);
+
+        List<BizEvent> bizEventItems = new ArrayList<>();
+        bizEventItems.add(generateValidBizEvent());
+
+        @SuppressWarnings("unchecked")
+        OutputBinding<List<Receipt>> documentdb = (OutputBinding<List<Receipt>>) spy(OutputBinding.class);
+
+        // test execution
+        assertDoesNotThrow(() -> function.processBizEventToReceipt(bizEventItems, documentdb, context));
+
+        verify(documentdb).setValue(receiptCaptor.capture());
+        Receipt captured = receiptCaptor.getValue().get(0);
+        assertEquals(ReceiptStatusType.NOT_QUEUE_SENT, captured.getStatus());
+        assertEquals(ReasonErrorCode.ERROR_QUEUE, captured.getReasonErr().getCode());
         assertEquals(EVENT_ID, captured.getIdEvent());
         assertEquals(PAYER_FISCAL_CODE, captured.getEventData().getPayerFiscalCode());
         assertEquals(DEBTOR_FISCAL_CODE, captured.getEventData().getDebtorFiscalCode());
