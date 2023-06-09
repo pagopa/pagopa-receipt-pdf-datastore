1635a18caf6aef6dd251390fe5d5a2a3b3238a6c
svariant <samuele.varianti@nttdata.com>
2023-06-09 12:38:28 +0200

    [PRDP-17] Completed unit tests for BizEventToReceipt function - defined QueueService class for sending messages to queue

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceipt.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceipt.java
@@ -1,29 +1,39 @@
 package it.gov.pagopa.receipt.pdf.datastore;
 
 import com.azure.core.http.rest.Response;
-import com.azure.storage.queue.QueueClient;
-import com.azure.storage.queue.QueueClientBuilder;
 import com.azure.storage.queue.models.SendMessageResult;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
-import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.annotation.CosmosDBOutput;
+import com.microsoft.azure.functions.annotation.CosmosDBTrigger;
+import com.microsoft.azure.functions.annotation.ExponentialBackoffRetry;
+import com.microsoft.azure.functions.annotation.FunctionName;
 import it.gov.pagopa.receipt.pdf.datastore.entities.event.BizEvent;
 import it.gov.pagopa.receipt.pdf.datastore.entities.event.enumeration.BizEventStatusType;
-import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReasonErrorCode;
-import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReceiptStatusType;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.EventData;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.ReasonError;
 import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.Receipt;
+import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReasonErrorCode;
+import it.gov.pagopa.receipt.pdf.datastore.entities.receipt.enumeration.ReceiptStatusType;
+import it.gov.pagopa.receipt.pdf.datastore.service.QueueService;
 import it.gov.pagopa.receipt.pdf.datastore.utils.ObjectMapperUtils;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class BizEventToReceipt {
+
+	private final QueueService queueService;
+
+	BizEventToReceipt(QueueService queueService){
+		this.queueService = queueService;
+	}
+
+	BizEventToReceipt(){
+		this.queueService = new QueueService();
+	}
 
 	@FunctionName("BizEventToReceiptProcessor")
 	@ExponentialBackoffRetry(maxRetryCount = 5, minimumInterval = "500", maximumInterval = "5000")
@@ -43,15 +53,9 @@ public class BizEventToReceipt {
 					collectionName = "receipts",
 					connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
 			OutputBinding<List<Receipt>> documentdb,
-			@QueueOutput(
-					name = "QueueReceiptWaitingForGen",
-					queueName = "COSMOS_RECEIPT_QUEUE_TOPIC",
-					connection = "COSMOS_RECEIPT_QUEUE_CONN_STRING")
-			OutputBinding<String> queueMessage,
 			final ExecutionContext context) {
 		
 		List<Receipt> itemsDone = new ArrayList<>();
-		List<BizEvent> itemsToNotify = new ArrayList<>();
 		Logger logger = context.getLogger();
 
 		String msg = String.format("BizEventEnrichment stat %s function - num events triggered %d", context.getInvocationId(),  items.size());
@@ -82,20 +86,26 @@ public class BizEventToReceipt {
 					String messageText = ObjectMapperUtils.writeValueAsString(be);
 
 					// Add a message to the queue
-					queueMessage.setValue(messageText);
+					Response<SendMessageResult> sendMessageResult = queueService.sendMessageToQueue(messageText);
 
-					receipt.setStatus(ReceiptStatusType.INSERTED);
+					if (sendMessageResult.getStatusCode() != 200) {
+						receipt.setStatus(ReceiptStatusType.NOT_QUEUE_SENT);
+						ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_QUEUE.getCode(), "Error sending message to queue");
+						receipt.setReasonErr(reasonError);
+					} else {
+						receipt.setStatus(ReceiptStatusType.INSERTED);
+					}
+
 
 				} catch (Exception e) {
 					receipt.setStatus(ReceiptStatusType.NOT_QUEUE_SENT);
-					ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_QUEUE.getCode(), "Error sending message to queue: "+e.getMessage());
+					ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_QUEUE.getCode(), "Error parsing BizEvent as string at: "+e.getMessage());
 					receipt.setReasonErr(reasonError);
 
-					logger.severe("Error sending msg to queue at "+ LocalDateTime.now()+ " : " + e.getMessage());
+					logger.severe("Error parsing BizEvent as string at "+ LocalDateTime.now()+ " : " + e.getMessage());
 				}
 
 				itemsDone.add(receipt);
-				itemsToNotify.add(be);
 
 			} else {
 				discarder++;
@@ -112,13 +122,7 @@ public class BizEventToReceipt {
 		// call the Datastore
 		msg = String.format("BizEventToReceipt stat %s function - number of receipts inserted on the datastore %d", context.getInvocationId(), itemsDone.size());
 		logger.info(msg);
-		try {
-			documentdb.setValue(itemsDone);
-		} catch (NullPointerException e) {
-			logger.severe("NullPointerException exception on cosmos receipts msg ingestion at "+ LocalDateTime.now()+ " : " + e.getMessage());
-		} catch (Exception e) {
-			logger.severe("Generic exception on cosmos receipts msg ingestion at "+ LocalDateTime.now()+ " : " + e.getMessage());
-		}
-	}
 
+		documentdb.setValue(itemsDone);
+	}
 }
