80abb1d4113dc836023c8073bf5c4f703e4d2fd1
svariant <samuele.varianti@nttdata.com>
2023-06-13 13:47:35 +0200

    [PRDP-17] Improved generate receipt function structure

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/GenerateReceipt.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/GenerateReceipt.java
@@ -4,30 +4,45 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
 import com.microsoft.azure.functions.annotation.*;
-import it.gov.pagopa.receipt.pdf.datastore.client.ReceiptCosmosClient;
+import it.gov.pagopa.receipt.pdf.datastore.client.PdfEngineClient;
+import it.gov.pagopa.receipt.pdf.datastore.client.PdfEngineClientProvider;
+import it.gov.pagopa.receipt.pdf.datastore.client.impl.PdfEngineClientProviderImpl;
+import it.gov.pagopa.receipt.pdf.datastore.client.impl.ReceiptCosmosClientImpl;
 import it.gov.pagopa.receipt.pdf.datastore.entity.event.BizEvent;
 import it.gov.pagopa.receipt.pdf.datastore.entity.receipt.ReasonError;
 import it.gov.pagopa.receipt.pdf.datastore.entity.receipt.Receipt;
 import it.gov.pagopa.receipt.pdf.datastore.entity.receipt.enumeration.ReasonErrorCode;
 import it.gov.pagopa.receipt.pdf.datastore.entity.receipt.enumeration.ReceiptStatusType;
+import it.gov.pagopa.receipt.pdf.datastore.model.request.PdfEngineRequest;
+import it.gov.pagopa.receipt.pdf.datastore.model.response.PdfEngineResponse;
 import it.gov.pagopa.receipt.pdf.datastore.exception.ReceiptNotFoundException;
 import it.gov.pagopa.receipt.pdf.datastore.utils.ObjectMapperUtils;
+import org.apache.commons.io.IOUtils;
 
+import javax.swing.text.html.HTML;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public class GenerateReceipt {
 
-    private ReceiptCosmosClient receiptCosmosClient;
+    private ReceiptCosmosClientImpl receiptCosmosClient;
 
-    GenerateReceipt(ReceiptCosmosClient receiptCosmosClient){
+    private PdfEngineClient pdfEngineClient;
+
+    GenerateReceipt(ReceiptCosmosClientImpl receiptCosmosClient, PdfEngineClient pdfEngineClient) {
         this.receiptCosmosClient = receiptCosmosClient;
+        this.pdfEngineClient = pdfEngineClient;
     }
 
-    GenerateReceipt(){
-        this.receiptCosmosClient = new ReceiptCosmosClient();
+    GenerateReceipt() {
+        this.receiptCosmosClient = new ReceiptCosmosClientImpl();
+        this.pdfEngineClient = new PdfEngineClientProviderImpl().provideClient();
     }
 
     @FunctionName("GenerateReceiptProcess")
@@ -66,13 +81,13 @@ public class GenerateReceipt {
             logger.severe("Error parsing bizEvent as string at " + LocalDateTime.now() + " : " + err.getMessage());
         }
 
+        //Retrieve receipt's data from CosmosDB
         Receipt receipt = null;
-        try{
+        try {
             receipt = receiptCosmosClient.getReceiptDocument(bizEvent.getId());
         } catch (ReceiptNotFoundException e) {
             requeueMessage.setValue(messageText);
         }
-
 
         if (receipt != null && receipt.getStatus().equals(ReceiptStatusType.INSERTED)) {
 
@@ -80,33 +95,54 @@ public class GenerateReceipt {
                     LocalDateTime.now(), bizEvent.getId(), bizEvent.getEventStatus());
             logger.info(logMsg);
 
-            //TODO verify if payer and debtor fiscal code are different or not
+            //Verify debtor's fiscal code is different from the payer's one
             String debtorCF = receipt.getEventData().getDebtorFiscalCode();
             String payerCF = receipt.getEventData().getPayerFiscalCode();
 
             boolean payerDebtorEqual = payerCF.equals(debtorCF);
 
-            for (int i = 0; i < (payerDebtorEqual ? 1 : 2); i++) {
-                //TODO verify if there is a pdf already generated (generate only the missing one)
-                switch (i) {
-                    case 0:
-                        if(receipt.getMdAttach().getName().isEmpty()){
-                            if(payerDebtorEqual){
-                                //TODO generate complete template
-                                handleReceiptPDFGeneration(receipt, 1, requeueMessage, messageText, logger);
-                            } else {
-                                //TODO generate partial template
-                                handleReceiptPDFGeneration(receipt, 2, requeueMessage, messageText, logger);
-                            }
-                        }
-                        break;
-                    case 1:
-                        if (receipt.getMdAttachPayer().getName().isEmpty()) {
-                            //TODO generate complete template
-                            handleReceiptPDFGeneration(receipt, 1, requeueMessage, messageText, logger);
-                        }
-                        break;
+            PdfEngineResponse statusDebtorGen = new PdfEngineResponse();
+            PdfEngineResponse statusPayerGen = new PdfEngineResponse();
+
+            //Generate PDFs for the needed fiscal codes
+            //TODO generate complete template
+            if (payerDebtorEqual) {
+                if (receipt.getMdAttach().getName().isEmpty()) {
+                    statusDebtorGen = handleReceiptPDFGeneration(receipt, true);
                 }
+            } else {
+                //TODO generate partial template
+                //verify the debot pdf hasn't already been generated
+                if (receipt.getMdAttach().getName().isEmpty()) {
+                    statusDebtorGen = handleReceiptPDFGeneration(receipt, false);
+                }
+                //TODO generate complete template
+                //verify the payer pdf hasn't already been generated
+                if (receipt.getMdAttachPayer().getName().isEmpty()) {
+                    statusPayerGen = handleReceiptPDFGeneration(receipt, true);
+                }
+            }
+
+            //Verify pdf generation success
+            if (statusDebtorGen.getStatusCode() == 200 && statusPayerGen.getStatusCode() == 200) {
+                //TODO update receipt with PDF metadata
+                receipt.setStatus(ReceiptStatusType.GENERATED);
+            } else {
+                //TODO menage in configuration max number of retry -> then FAILED
+                if (receipt.getNumRetry() > 5) {
+                    receipt.setStatus(ReceiptStatusType.FAILED);
+                } else {
+                    receipt.setStatus(ReceiptStatusType.RETRY);
+                }
+                //TODO menage api call error code -> pdfengineStatusCode + add PDFEngine error message
+                int errorStatusCode = statusDebtorGen.getStatusCode() == 200 ? statusPayerGen.getStatusCode() : statusDebtorGen.getStatusCode();
+                ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_PDF_ENGINE.getCustomCode(errorStatusCode), "Error generating PDF: ");
+                receipt.setReasonErr(reasonError);
+                receipt.setNumRetry(receipt.getNumRetry() + 1);
+
+                requeueMessage.setValue(messageText);
+                //TODO add PDFEngine error message
+                logger.severe("Error generating PDF at " + LocalDateTime.now() + " : ");
             }
 
             itemsToNotify.add(receipt);
@@ -124,33 +160,31 @@ public class GenerateReceipt {
         }
     }
 
-    private void handleReceiptPDFGeneration(Receipt receipt, int templateType, OutputBinding<String> requeueMessage, String messageText, Logger logger){
+    private PdfEngineResponse handleReceiptPDFGeneration(Receipt receipt, boolean completeTemplate) {
         int pdfEngineStatusCode = 0;
+        PdfEngineRequest request = new PdfEngineRequest();
+        PdfEngineResponse response = new PdfEngineResponse();
+        String templateFileName = completeTemplate ? "/complete_template.html" : "/partial_template.html";
 
-            //TODO add call to PDFEngine service to generate pdf (full o partial template)
+        String htmlTemplate = null;
+        try {
+            htmlTemplate = IOUtils.toString(
+                    Objects.requireNonNull(this.getClass().getResourceAsStream(templateFileName)),
+                    StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            response.setStatusCode(400);
 
-        if(pdfEngineStatusCode == 200){
-            //TODO update receipt with PDF metadata
-            receipt.setStatus(ReceiptStatusType.GENERATED);
-        } else {
-            //TODO menage max number of retry -> then FAILED
-            if(receipt.getNumRetry() > 5){
-                receipt.setStatus(ReceiptStatusType.FAILED);
-            } else {
-                receipt.setStatus(ReceiptStatusType.RETRY);
-            }
-            //TODO menage api call error code -> pdfengineStatusCode + add PDFEngine error message
-            ReasonError reasonError = new ReasonError(ReasonErrorCode.ERROR_PDF_ENGINE.getCustomCode(pdfEngineStatusCode), "Error generating PDF: " );
-            receipt.setReasonErr(reasonError);
-            receipt.setNumRetry(receipt.getNumRetry() + 1);
-
-            requeueMessage.setValue(messageText);
-            //TODO add PDFEngine error message
-            logger.severe("Error generating PDF at " + LocalDateTime.now() + " : ");
+            return response;
         }
 
+        request.setTemplate(htmlTemplate);
 
+        response = pdfEngineClient.generatePDF(request);
 
+        //TODO add call to PDFEngine service to generate pdf (full o partial template)
 
+        response.setStatusCode(pdfEngineStatusCode);
+
+        return response;
     }
 }
