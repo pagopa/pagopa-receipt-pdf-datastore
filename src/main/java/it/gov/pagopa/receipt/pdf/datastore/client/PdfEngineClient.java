ea748a80edc8c51e2a9dcf18e54f0c7f4ed3c553
svariant <samuele.varianti@nttdata.com>
2023-06-14 09:59:11 +0200

    [PRDP-17] Changed request body to PDF engine

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/client/PdfEngineClient.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/client/PdfEngineClient.java
@@ -2,12 +2,12 @@ package it.gov.pagopa.receipt.pdf.datastore.client;
 
 import feign.Headers;
 import feign.RequestLine;
+import feign.Response;
 import it.gov.pagopa.receipt.pdf.datastore.model.request.PdfEngineRequest;
-import it.gov.pagopa.receipt.pdf.datastore.model.response.PdfEngineResponse;
 
 public interface PdfEngineClient {
 
     @RequestLine("POST /generate-pdf")
-    @Headers("Content-type: application/json")
-    PdfEngineResponse generatePDF(PdfEngineRequest request);
+    @Headers("Content-type: multipart/form-data")
+    Response generatePDF(PdfEngineRequest request);
 }
--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/model/request/PdfEngineRequest.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/model/request/PdfEngineRequest.java
@@ -3,11 +3,14 @@ package it.gov.pagopa.receipt.pdf.datastore.model.request;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.File;
+import java.util.Map;
+
 @Setter
 @NoArgsConstructor
 public class PdfEngineRequest {
 
-    String template;
-    String data;
+    File template;
+    Map<String, Object> data;
     boolean applySignature;
 }
