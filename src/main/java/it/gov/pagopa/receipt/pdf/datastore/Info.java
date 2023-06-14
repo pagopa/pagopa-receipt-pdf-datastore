82aba02649f1cbb4f1a834c671f12b37207c1ba7
svariant <samuele.varianti@nttdata.com>
2023-06-14 12:02:47 +0200

    [PRDP-17] Fixed package name Info file

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/Info.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/Info.java
@@ -1,4 +1,4 @@
-package it.gov.pagopa.bizeventsdatastore;
+package it.gov.pagopa.receipt.pdf.datastore;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
