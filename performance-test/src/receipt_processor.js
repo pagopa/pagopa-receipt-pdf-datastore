5b3d73a8a6a06a306967c7c928c25aea3db734ee
svariant <128470180+svariant@users.noreply.github.com>
2023-07-21 15:29:10 +0200

    [PRDP-23] feat: added performance tests (#27)
    * [PRDP-23] feat: add performance tests
    
    * [PRDP-23] feat: updated receipt_processor.js
    
    * PRDP-23: updated environment variables and fixed scripts
    
    * [PRDP-23] Defined expressJS app service called by the k6 tests to create documents on CosmosDB
    
    * [PRDP-23] Added delete and getDocument to cosmos service
    
    * [PRDP-23] Added document status log
    
    * [PRDP-23] Updated README
    
    * [PRDP-23] Changed README title
    
    * [PRDP-23] Formatted README
    
    * [PRDP-23] Added cosmos service to docker compose
    
    * [PRDP-23] Generalized getDocument
    
    * [PRDP-23] Re-changed cosmos api calls to k6 http call with fix - commented calls to custom service
    
    * [PRDP-23] Deleted cosmos service app
    
    * [PRDP-23] Fixed receipt status type
    
    * [PRDP-23] chore: updated values (deactivating canary deployment for datastore)
    
    * [PRDP-23] Azure Pipeline - changed process waiting time
    
    * [PRDP-23] Changed cosmos sub key to secret var
    
    * [PRDP-23] Changed test type default to "load"
    
    * [PRDP-23] Fixed script name
    
    * [PRDP-23] Changed process time to 5 s
    
    * [PRDP-23] Changed dbname
    
    * [PRDP-23] Changed http server to uat
    
    * [PRDP-23] Changed environment by env var in nginx config
    
    * [PRDP-23] Changed back nginx env
    
    * [feat-remove-canary] Removed canary deploy
    
    * [PRDP-23] Fixed env values
    
    ---------
    
    Co-authored-by: Alessio Cialini <acialini@external.nexigroup.com>
    Co-authored-by: giomella <gioele.mella.st@everis.nttdata.com>

---
+++ performance-test/src/receipt_processor.js
@@ -0,0 +1,96 @@
+import { sleep, check } from 'k6';
+import { SharedArray } from 'k6/data';
+
+import { randomString, createEvent } from './modules/common.js'
+import { createDocument, deleteDocument, getDocumentByEventId } from "./modules/datastore_client.js";
+
+export let options = JSON.parse(open(__ENV.TEST_TYPE));
+
+// read configuration
+// note: SharedArray can currently only be constructed inside init code
+// according to https://k6.io/docs/javascript-api/k6-data/sharedarray
+const varsArray = new SharedArray('vars', function () {
+	return JSON.parse(open(`./${__ENV.VARS}`)).environment;
+});
+// workaround to use shared array (only array should be used)
+const vars = varsArray[0];
+const bizEventCosmosDBURI = `${vars.bizEventCosmosDBURI}`;
+const bizEventDatabaseID = `${vars.bizEventDatabaseID}`;
+const bizEventContainerID = `${vars.bizEventContainerID}`;
+const bizEventCosmosDBPrimaryKey = `${__ENV.BIZEVENT_COSMOS_DB_SUBSCRIPTION_KEY}`;
+const receiptCosmosDBURI = `${vars.receiptCosmosDBURI}`;
+const receiptDatabaseID = `${vars.receiptDatabaseID}`;
+const receiptContainerID = `${vars.receiptContainerID}`;
+const receiptCosmosDBPrimaryKey = `${__ENV.RECEIPT_COSMOS_DB_SUBSCRIPTION_KEY}`;
+// boundary time (s) to process event: activate trigger, process function, upload event to datastore
+const processTime = `${vars.processTime}`;
+
+export function setup() {
+	// 2. setup code (once)
+	// The setup code runs, setting up the test environment (optional) and generating data
+	// used to reuse code for the same VU
+
+	// todo
+
+	// precondition is moved to default fn because in this stage
+	// __VU is always 0 and cannot be used to create env properly
+}
+
+// teardown the test data
+export function teardown(data) {
+	// todo
+}
+
+function postcondition(eventId) {
+	// verify that published event have been stored properly in the datastore
+	let tag = { datastoreMethod: "GetDocumentByEventId" };
+	let r = getDocumentByEventId(receiptCosmosDBURI, receiptDatabaseID, receiptContainerID, receiptCosmosDBPrimaryKey, eventId);
+
+	console.log("GetDocumentByEventId call, Status " + r.status);
+
+	let { _count, Documents } = r.json();
+
+	let receipt = Documents[0];
+
+	check(r, {
+		"Assert published receipt is in the datastore and with status GENERATED or beyond": (_r) => _count === 1 && (
+			receipt.status !== "INSERTED" &&
+			receipt.status !== "FAILED" &&
+			receipt.status !== "NOT_QUEUE_SENT" &&
+			receipt.status !== "RETRY"
+		) &&
+			receipt.mdAttach
+			&&
+			receipt.mdAttach.url
+		,
+	}, tag);
+
+	if (_count) {
+		let receiptId = receipt.id;
+
+		deleteDocument(bizEventCosmosDBURI, bizEventDatabaseID, bizEventContainerID, bizEventCosmosDBPrimaryKey, eventId);
+		deleteDocument(receiptCosmosDBURI, receiptDatabaseID, receiptContainerID, receiptCosmosDBPrimaryKey, receiptId);
+	}
+}
+
+export default function () {
+	// publish event
+	let tag = { eventHubMethod: "SaveBizEvent" };
+	const id = randomString(15, "abcdefghijklmnopqrstuvwxyz0123456789");
+	let event = createEvent(id);
+
+	//	let r = createDocument(bizEventCosmosDBURI, bizEventDatabaseID, bizEventContainerID, bizEventCosmosDBPrimaryKey, event, id);
+	let r = createDocument(bizEventCosmosDBURI, bizEventDatabaseID, bizEventContainerID, bizEventCosmosDBPrimaryKey, event, id);
+
+	console.log("PublishEvent call, Status " + r.status);
+
+	check(r, {
+		'PublishEvent status is 201': (_response) => r.status === 201,
+	}, tag);
+
+	// if the event is published wait and check if it was correctly processed and stored in the datastore
+	if (r.status === 201) {
+		sleep(processTime);
+		postcondition(id);
+	}
+}
