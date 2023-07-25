6cebd386eefa6048ae0be0937efc4a4dc75c7fbd
Alessio Cialini <acialini@external.nexigroup.com>
2023-07-25 11:02:12 +0200

    [PRDP-75] Updated Dockerfile and values-dev.yaml

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceipt.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/BizEventToReceipt.java
@@ -23,114 +23,114 @@ import java.util.logging.Logger;
  */
 public class BizEventToReceipt {
 
-    /**
-     * This function will be invoked when an CosmosDB trigger occurs
-     * #
-     * A receipt object is generated from the biz-event's data
-     * The biz-event is encoded in a base64 string and sent as message to a queue
-     * The receipt is saved to a specific Cosmos database with the following status:
-     * - INSERTED if sending the message to the queue succeeded
-     * - NOT_QUEUE_SENT if sending the message to the queue failed
-     * #
-     *
-     * @param items      Biz-events that triggered the function from the Cosmos database
-     * @param documentdb Output binding that will save the receipt data retrieved from the biz-events
-     * @param context    Function context
-     */
-    @FunctionName("BizEventToReceiptProcessor")
-    @ExponentialBackoffRetry(maxRetryCount = 5, minimumInterval = "500", maximumInterval = "5000")
-    public void processBizEventToReceipt(
-            @CosmosDBTrigger(
-                    name = "BizEventDatastore",
-                    databaseName = "db",
-                    collectionName = "biz-events",
-                    leaseCollectionName = "biz-events-leases",
-                    leaseCollectionPrefix = "materialized",
-                    createLeaseCollectionIfNotExists = true,
-                    maxItemsPerInvocation = 100,
-                    connectionStringSetting = "COSMOS_BIZ_EVENT_CONN_STRING")
-            List<BizEvent> items,
-            @CosmosDBOutput(
-                    name = "ReceiptDatastore",
-                    databaseName = "db",
-                    collectionName = "receipts",
-                    connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
-            OutputBinding<List<Receipt>> documentdb,
-            final ExecutionContext context) {
-
-        List<Receipt> itemsDone = new ArrayList<>();
-        Logger logger = context.getLogger();
-
-        String msg = String.format("[%s] stat %s function - num events triggered %d", context.getFunctionName(),
-                context.getInvocationId(), items.size());
-        logger.fine(msg);
-        int discarder = 0;
-
-        //Retrieve receipt data from biz-event
-        for (BizEvent bizEvent : items) {
-
-            try {
-                //Process only biz-event in status DONE
-                if (bizEvent != null && bizEvent.getEventStatus().equals(BizEventStatusType.DONE)) {
-                    BizEventToReceiptService service = new BizEventToReceiptService();
-
-                    Receipt receipt = new Receipt();
-
-                    //Insert biz-event data into receipt
-                    receipt.setEventId(bizEvent.getId());
-
-                    EventData eventData = new EventData();
-                    //TODO verify if payer's or debtor's CF can be null
-                    eventData.setPayerFiscalCode(bizEvent.getPayer() != null ? bizEvent.getPayer().getEntityUniqueIdentifierValue() : null);
-                    eventData.setDebtorFiscalCode(bizEvent.getDebtor() != null ? bizEvent.getDebtor().getEntityUniqueIdentifierValue() : null);
-                    eventData.setTransactionCreationDate(
-                            service.getTransactionCreationDate(bizEvent)
-                    );
-
-                    receipt.setEventData(eventData);
-
-                    String message = String.format("[%s] function called at %s for event with id %s and status %s",
-                            context.getFunctionName(), LocalDateTime.now(), bizEvent.getId(), bizEvent.getEventStatus());
-                    logger.info(message);
-
-                    //Send biz event as message to queue (to be processed from the other function)
-                    service.handleSendMessageToQueue(bizEvent, receipt, logger);
-
-                    //Add receipt to items to be saved on CosmosDB
-                    itemsDone.add(receipt);
-
-                } else {
-                    //Discard biz events not in status DONE
-                    discarder++;
-                    msg = String.format("[%s] event with id %s discarded because in status %s",
-                            context.getFunctionName(), bizEvent.getId(), bizEvent.getEventStatus());
-                    logger.fine(msg);
-                }
-            } catch (Exception e) {
-                discarder++;
-                //Error info
-                msg = String.format("[%s] Error to process event with id %s", context.getFunctionName(), bizEvent.getId());
-                logger.log(Level.SEVERE, msg, e);
-            }
-        }
-        //Discarder info
-        msg = String.format("[%s] itemsDone stat %s function - %d number of events in discarder", context.getFunctionName(),
-                context.getInvocationId(), discarder);
-        logger.fine(msg);
-
-        //Call to queue info
-        msg = String.format("[%s] itemsDone stat %s function - number of events in DONE sent to the receipt queue %d",
-                context.getFunctionName(), context.getInvocationId(), itemsDone.size());
-        logger.fine(msg);
-
-        //Call to datastore info
-        msg = String.format("[%s] stat %s function - number of receipts inserted on the datastore %d", context.getFunctionName(),
-                context.getInvocationId(), itemsDone.size());
-        logger.fine(msg);
-
-        //Save receipts data to CosmosDB
-        if (!itemsDone.isEmpty()) {
-            documentdb.setValue(itemsDone);
-        }
-    }
+//    /**
+//     * This function will be invoked when an CosmosDB trigger occurs
+//     * #
+//     * A receipt object is generated from the biz-event's data
+//     * The biz-event is encoded in a base64 string and sent as message to a queue
+//     * The receipt is saved to a specific Cosmos database with the following status:
+//     * - INSERTED if sending the message to the queue succeeded
+//     * - NOT_QUEUE_SENT if sending the message to the queue failed
+//     * #
+//     *
+//     * @param items      Biz-events that triggered the function from the Cosmos database
+//     * @param documentdb Output binding that will save the receipt data retrieved from the biz-events
+//     * @param context    Function context
+//     */
+//    @FunctionName("BizEventToReceiptProcessor")
+//    @ExponentialBackoffRetry(maxRetryCount = 5, minimumInterval = "500", maximumInterval = "5000")
+//    public void processBizEventToReceipt(
+//            @CosmosDBTrigger(
+//                    name = "BizEventDatastore",
+//                    databaseName = "db",
+//                    collectionName = "biz-events",
+//                    leaseCollectionName = "biz-events-leases",
+//                    leaseCollectionPrefix = "materialized",
+//                    createLeaseCollectionIfNotExists = true,
+//                    maxItemsPerInvocation = 100,
+//                    connectionStringSetting = "COSMOS_BIZ_EVENT_CONN_STRING")
+//            List<BizEvent> items,
+//            @CosmosDBOutput(
+//                    name = "ReceiptDatastore",
+//                    databaseName = "db",
+//                    collectionName = "receipts",
+//                    connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
+//            OutputBinding<List<Receipt>> documentdb,
+//            final ExecutionContext context) {
+//
+//        List<Receipt> itemsDone = new ArrayList<>();
+//        Logger logger = context.getLogger();
+//
+//        String msg = String.format("[%s] stat %s function - num events triggered %d", context.getFunctionName(),
+//                context.getInvocationId(), items.size());
+//        logger.fine(msg);
+//        int discarder = 0;
+//
+//        //Retrieve receipt data from biz-event
+//        for (BizEvent bizEvent : items) {
+//
+//            try {
+//                //Process only biz-event in status DONE
+//                if (bizEvent != null && bizEvent.getEventStatus().equals(BizEventStatusType.DONE)) {
+//                    BizEventToReceiptService service = new BizEventToReceiptService();
+//
+//                    Receipt receipt = new Receipt();
+//
+//                    //Insert biz-event data into receipt
+//                    receipt.setEventId(bizEvent.getId());
+//
+//                    EventData eventData = new EventData();
+//                    //TODO verify if payer's or debtor's CF can be null
+//                    eventData.setPayerFiscalCode(bizEvent.getPayer() != null ? bizEvent.getPayer().getEntityUniqueIdentifierValue() : null);
+//                    eventData.setDebtorFiscalCode(bizEvent.getDebtor() != null ? bizEvent.getDebtor().getEntityUniqueIdentifierValue() : null);
+//                    eventData.setTransactionCreationDate(
+//                            service.getTransactionCreationDate(bizEvent)
+//                    );
+//
+//                    receipt.setEventData(eventData);
+//
+//                    String message = String.format("[%s] function called at %s for event with id %s and status %s",
+//                            context.getFunctionName(), LocalDateTime.now(), bizEvent.getId(), bizEvent.getEventStatus());
+//                    logger.info(message);
+//
+//                    //Send biz event as message to queue (to be processed from the other function)
+//                    service.handleSendMessageToQueue(bizEvent, receipt, logger);
+//
+//                    //Add receipt to items to be saved on CosmosDB
+//                    itemsDone.add(receipt);
+//
+//                } else {
+//                    //Discard biz events not in status DONE
+//                    discarder++;
+//                    msg = String.format("[%s] event with id %s discarded because in status %s",
+//                            context.getFunctionName(), bizEvent.getId(), bizEvent.getEventStatus());
+//                    logger.fine(msg);
+//                }
+//            } catch (Exception e) {
+//                discarder++;
+//                //Error info
+//                msg = String.format("[%s] Error to process event with id %s", context.getFunctionName(), bizEvent.getId());
+//                logger.log(Level.SEVERE, msg, e);
+//            }
+//        }
+//        //Discarder info
+//        msg = String.format("[%s] itemsDone stat %s function - %d number of events in discarder", context.getFunctionName(),
+//                context.getInvocationId(), discarder);
+//        logger.fine(msg);
+//
+//        //Call to queue info
+//        msg = String.format("[%s] itemsDone stat %s function - number of events in DONE sent to the receipt queue %d",
+//                context.getFunctionName(), context.getInvocationId(), itemsDone.size());
+//        logger.fine(msg);
+//
+//        //Call to datastore info
+//        msg = String.format("[%s] stat %s function - number of receipts inserted on the datastore %d", context.getFunctionName(),
+//                context.getInvocationId(), itemsDone.size());
+//        logger.fine(msg);
+//
+//        //Save receipts data to CosmosDB
+//        if (!itemsDone.isEmpty()) {
+//            documentdb.setValue(itemsDone);
+//        }
+//    }
 }
--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/RetryReviewedPoisonMessages.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/RetryReviewedPoisonMessages.java
@@ -22,81 +22,81 @@ import java.util.logging.Logger;
  */
 public class RetryReviewedPoisonMessages {
 
-    /**
-     * This function will be invoked when an CosmosDB trigger occurs
-     *
-     * When an updated document in the receipt-message-errors CosmosDB has status REVIEWED attempts
-     * to send it back to the provided output topic.
-     * If succeeds saves the element with status REQUEUED
-     * If fails updates the document back in status TO_REVIEW with an updated error description
-     *
-     * @param items      Reviewed Receipt Errors that triggered the function from the Cosmos database
-     * @param context    Function context
-     */
-    @FunctionName("RetryReviewedPoisonMessagesProcessor")
-    @ExponentialBackoffRetry(maxRetryCount = 5, minimumInterval = "500", maximumInterval = "5000")
-    public void processRetryReviewedPoisonMessages(
-            @CosmosDBTrigger(
-                    name = "ReceiptErrorDatastore",
-                    databaseName = "db",
-                    collectionName = "receipts-message-errors",
-                    leaseCollectionName = "receipts-message-errors-leases",
-                    leaseCollectionPrefix = "materialized",
-                    createLeaseCollectionIfNotExists = true,
-                    maxItemsPerInvocation = 100,
-                    connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
-            List<ReceiptError> items,
-            @CosmosDBOutput(
-                    name = "ReceiptMessageErrorsDatastoreOutput",
-                    databaseName = "db",
-                    collectionName = "receipts-message-errors",
-                    connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
-            OutputBinding<List<ReceiptError>> documentdb,
-            final ExecutionContext context) {
-
-        List<ReceiptError> itemsDone = new ArrayList<>();
-        Logger logger = context.getLogger();
-
-        String msg = String.format("[%s] documentCaptorValue stat %s function - num errors reviewed triggered %d",
-                context.getFunctionName(), context.getInvocationId(), items.size());
-        logger.fine(msg);
-
-        ReceiptQueueClientImpl queueService = ReceiptQueueClientImpl.getInstance();
-
-        //Retrieve receipt data from biz-event
-        for (ReceiptError receiptError : items) {
-
-                //Process only errors in status REVIEWED
-                if (receiptError != null && receiptError.getStatus().equals(ReceiptErrorStatusType.REVIEWED)) {
-
-                    try {
-
-                        Response<SendMessageResult> sendMessageResult =
-                            queueService.sendMessageToQueue(Base64.getMimeEncoder().encodeToString(
-                                    receiptError.getMessagePayload().getBytes()));
-                        if (sendMessageResult.getStatusCode() != HttpStatus.CREATED.value()) {
-                            throw new UnableToQueueException("Unable to queue due to error: " +
-                                    sendMessageResult.getStatusCode());
-                        }
-
-                        receiptError.setStatus(ReceiptErrorStatusType.REQUEUED);
-
-                    } catch (Exception e) {
-                        //Error info
-                        msg = String.format("[%s] Error to process receiptError with id %s",
-                                context.getFunctionName(), receiptError.getId());
-                        logger.log(Level.SEVERE, msg, e);
-                        receiptError.setMessageError(e.getMessage());
-                        receiptError.setStatus(ReceiptErrorStatusType.TO_REVIEW);
-                    }
-
-                    itemsDone.add(receiptError);
-
-               }
-
-        }
-
-        documentdb.setValue(itemsDone);
-
-    }
+//    /**
+//     * This function will be invoked when an CosmosDB trigger occurs
+//     *
+//     * When an updated document in the receipt-message-errors CosmosDB has status REVIEWED attempts
+//     * to send it back to the provided output topic.
+//     * If succeeds saves the element with status REQUEUED
+//     * If fails updates the document back in status TO_REVIEW with an updated error description
+//     *
+//     * @param items      Reviewed Receipt Errors that triggered the function from the Cosmos database
+//     * @param context    Function context
+//     */
+//    @FunctionName("RetryReviewedPoisonMessagesProcessor")
+//    @ExponentialBackoffRetry(maxRetryCount = 5, minimumInterval = "500", maximumInterval = "5000")
+//    public void processRetryReviewedPoisonMessages(
+//            @CosmosDBTrigger(
+//                    name = "ReceiptErrorDatastore",
+//                    databaseName = "db",
+//                    collectionName = "receipts-message-errors",
+//                    leaseCollectionName = "receipts-message-errors-leases",
+//                    leaseCollectionPrefix = "materialized",
+//                    createLeaseCollectionIfNotExists = true,
+//                    maxItemsPerInvocation = 100,
+//                    connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
+//            List<ReceiptError> items,
+//            @CosmosDBOutput(
+//                    name = "ReceiptMessageErrorsDatastoreOutput",
+//                    databaseName = "db",
+//                    collectionName = "receipts-message-errors",
+//                    connectionStringSetting = "COSMOS_RECEIPTS_CONN_STRING")
+//            OutputBinding<List<ReceiptError>> documentdb,
+//            final ExecutionContext context) {
+//
+//        List<ReceiptError> itemsDone = new ArrayList<>();
+//        Logger logger = context.getLogger();
+//
+//        String msg = String.format("[%s] documentCaptorValue stat %s function - num errors reviewed triggered %d",
+//                context.getFunctionName(), context.getInvocationId(), items.size());
+//        logger.fine(msg);
+//
+//        ReceiptQueueClientImpl queueService = ReceiptQueueClientImpl.getInstance();
+//
+//        //Retrieve receipt data from biz-event
+//        for (ReceiptError receiptError : items) {
+//
+//                //Process only errors in status REVIEWED
+//                if (receiptError != null && receiptError.getStatus().equals(ReceiptErrorStatusType.REVIEWED)) {
+//
+//                    try {
+//
+//                        Response<SendMessageResult> sendMessageResult =
+//                            queueService.sendMessageToQueue(Base64.getMimeEncoder().encodeToString(
+//                                    receiptError.getMessagePayload().getBytes()));
+//                        if (sendMessageResult.getStatusCode() != HttpStatus.CREATED.value()) {
+//                            throw new UnableToQueueException("Unable to queue due to error: " +
+//                                    sendMessageResult.getStatusCode());
+//                        }
+//
+//                        receiptError.setStatus(ReceiptErrorStatusType.REQUEUED);
+//
+//                    } catch (Exception e) {
+//                        //Error info
+//                        msg = String.format("[%s] Error to process receiptError with id %s",
+//                                context.getFunctionName(), receiptError.getId());
+//                        logger.log(Level.SEVERE, msg, e);
+//                        receiptError.setMessageError(e.getMessage());
+//                        receiptError.setStatus(ReceiptErrorStatusType.TO_REVIEW);
+//                    }
+//
+//                    itemsDone.add(receiptError);
+//
+//               }
+//
+//        }
+//
+//        documentdb.setValue(itemsDone);
+//
+//    }
 }
