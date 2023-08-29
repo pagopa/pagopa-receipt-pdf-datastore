cf6ad447226673fadfb6e50f882922a81b067025
svariant <samuele.varianti@nttdata.com>
2023-08-29 12:33:28 +0200

    [PRDP-117] Fixed missing curl bracket

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -55,6 +55,7 @@ data "azurerm_cosmosdb_account" "receipts_cosmos" {
 data "azurerm_cosmosdb_account" "bizevents_cosmos" {
   name                = "pagopa-${var.env_short}-${local.location_short}-bizevents-ds-cosmos-account"
   resource_group_name = "pagopa-${var.env_short}-${local.location_short}-bizevents-rg"
+}
 
 data "azurerm_key_vault_secret" "key_vault_integration_test_webhook_slack" {
   name         = "webhook-slack"
