8e9366ef10f5fd9d9106895aa871b27f4ef8c548
svariant <samuele.varianti@nttdata.com>
2023-09-15 16:57:50 +0200

    [PRDP-136] Test added log env

--- performance-test/src/scripts/scripts_common.js
+++ performance-test/src/scripts/scripts_common.js
@@ -21,6 +21,8 @@ const bizEventContainerID = environmentVars.bizEventContainerID;
 const receiptDatabaseID = environmentVars.receiptDatabaseID;
 const receiptContainerID = environmentVars.receiptContainerID;
 
+console.log(blobStorageConnString, receiptCosmosDBConnString, bizEventCosmosDBConnString, bizEventContainerID, bizEventDatabaseID, receiptDatabaseID, receiptContainerID, receiptCosmosDBConnString);
+
 //CLIENTS
 const blobServiceClient = BlobServiceClient.fromConnectionString(blobStorageConnString || "");
 export const blobContainerClient = blobServiceClient.getContainerClient(blobStorageContainerID || "");
