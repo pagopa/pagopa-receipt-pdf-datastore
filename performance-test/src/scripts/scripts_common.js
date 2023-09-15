f29fb560c2feb2e608f79f9bf5cc53380254333d
svariant <samuele.varianti@nttdata.com>
2023-09-15 17:29:38 +0200

    [PRDP-136] Fixed environment env

--- performance-test/src/scripts/scripts_common.js
+++ performance-test/src/scripts/scripts_common.js
@@ -21,7 +21,6 @@ const bizEventContainerID = environmentVars.bizEventContainerID;
 const receiptDatabaseID = environmentVars.receiptDatabaseID;
 const receiptContainerID = environmentVars.receiptContainerID;
 
-console.log(blobStorageConnString, blobStorageContainerID);
 //CLIENTS
 const blobServiceClient = BlobServiceClient.fromConnectionString(blobStorageConnString || "");
 export const blobContainerClient = blobServiceClient.getContainerClient(blobStorageContainerID || "");
