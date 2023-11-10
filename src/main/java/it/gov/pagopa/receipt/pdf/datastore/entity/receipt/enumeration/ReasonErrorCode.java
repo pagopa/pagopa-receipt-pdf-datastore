0fb4cbd09924a592256b52effafc11a1d7906d3b
svariant <128470180+svariant@users.noreply.github.com>
2023-11-10 15:03:10 +0100

    [fix] Updated reason error codes (#89)

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/entity/receipt/enumeration/ReasonErrorCode.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/entity/receipt/enumeration/ReasonErrorCode.java
@@ -1,9 +1,13 @@
 package it.gov.pagopa.receipt.pdf.datastore.entity.receipt.enumeration;
 
 public enum ReasonErrorCode {
-    ERROR_QUEUE(902), ERROR_BLOB_STORAGE(901), ERROR_PDF_ENGINE(0), ERROR_PDV_TOKENIZER(701);
+    ERROR_QUEUE(902),
+    ERROR_PDV_IO(800),
+    ERROR_PDV_UNEXPECTED(801),
+    ERROR_PDV_MAPPING(802);
+    ;
 
-    private int code;
+    private final int code;
 
     ReasonErrorCode(int code){
         this.code = code;
@@ -11,9 +15,5 @@ public enum ReasonErrorCode {
 
     public int getCode(){
         return this.code;
-    }
-
-    public int getCustomCode(int customCode){
-        return customCode;
     }
 }
