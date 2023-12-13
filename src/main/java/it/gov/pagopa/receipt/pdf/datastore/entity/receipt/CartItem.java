830bf0192e32014f6dc4001321a64c2a350d1b4d
giomella <gioele.mella@emeal.nttdata.com>
2023-12-13 12:35:59 +0100

    [PRDP-274] updated cart function logic and added unit tests

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/entity/receipt/CartItem.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/entity/receipt/CartItem.java
@@ -1,11 +1,13 @@
 package it.gov.pagopa.receipt.pdf.datastore.entity.receipt;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class CartItem {
     private String subject;
