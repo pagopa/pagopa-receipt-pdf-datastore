5de4b0fc5cfc3395f33aa71287470679e03d8359
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-10-27 21:48:40 +0100

    coverage

--- src/test/java/it/gov/pagopa/receipt/pdf/datastore/client/impl/BizEventCosmosClientImplTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/datastore/client/impl/BizEventCosmosClientImplTest.java
@@ -5,6 +5,7 @@ import com.azure.cosmos.CosmosContainer;
 import com.azure.cosmos.CosmosDatabase;
 import com.azure.cosmos.util.CosmosPagedIterable;
 import it.gov.pagopa.receipt.pdf.datastore.entity.event.BizEvent;
+import it.gov.pagopa.receipt.pdf.datastore.exception.BizEventNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -73,5 +74,30 @@ class BizEventCosmosClientImplTest {
         BizEventCosmosClientImpl client = new BizEventCosmosClientImpl(mockClient);
 
         Assertions.assertDoesNotThrow(() -> client.getBizEventDocument("1"));
+    }
+    @Test
+    void getBizEventDocumentError() {
+        CosmosClient mockClient = mock(CosmosClient.class);
+
+        CosmosDatabase mockDatabase = mock(CosmosDatabase.class);
+        CosmosContainer mockContainer = mock(CosmosContainer.class);
+
+        CosmosPagedIterable mockIterable = mock(CosmosPagedIterable.class);
+
+        Iterator<BizEvent> mockIterator = mock(Iterator.class);
+        BizEvent bizEvent = new BizEvent();
+
+        when(mockIterator.hasNext()).thenReturn(false);
+        when(mockIterator.next()).thenReturn(bizEvent);
+
+        when(mockIterable.iterator()).thenReturn(mockIterator);
+
+        when(mockContainer.queryItems(anyString(), any(), eq(BizEvent.class))).thenReturn(mockIterable);
+        when(mockDatabase.getContainer(any())).thenReturn(mockContainer);
+        when(mockClient.getDatabase(any())).thenReturn(mockDatabase);
+
+        BizEventCosmosClientImpl client = new BizEventCosmosClientImpl(mockClient);
+
+        Assertions.assertThrows(BizEventNotFoundException.class, () -> client.getBizEventDocument("1"));
     }
 }
