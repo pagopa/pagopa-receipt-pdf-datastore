e61184fb01ba6b0b1c30476f244ab65bcf2614b2
Jacopo Carlini <jacopo.carlini@gmail.com>
2026-01-21 17:24:51 +0100

    test: fix junit

--- src/test/java/it/gov/pagopa/receipt/pdf/datastore/service/impl/CartReceiptCosmosServiceImplTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/datastore/service/impl/CartReceiptCosmosServiceImplTest.java
@@ -5,8 +5,11 @@ import it.gov.pagopa.receipt.pdf.datastore.client.impl.ReceiptCosmosClientImpl;
 import it.gov.pagopa.receipt.pdf.datastore.entity.cart.CartForReceipt;
 import it.gov.pagopa.receipt.pdf.datastore.exception.CartNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,6 +18,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 
+@ExtendWith({MockitoExtension.class, SystemStubsExtension.class})
 class CartReceiptCosmosServiceImplTest {
 
     @Mock
