216f95f1f07df78f8e409f4fd723a2479446d3d3
Jacopo Carlini <jacopo.carlini@gmail.com>
2026-07-02 10:23:38 +0200

    removed unsued import

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/utils/BizEventToReceiptUtils.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/utils/BizEventToReceiptUtils.java
@@ -25,7 +25,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
