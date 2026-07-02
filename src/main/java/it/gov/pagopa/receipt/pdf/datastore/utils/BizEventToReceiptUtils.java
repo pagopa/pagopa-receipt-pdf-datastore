e2a54521514f62206b2ccb762bc53397bd0f791b
Jacopo Carlini <jacopo.carlini@gmail.com>
2026-07-02 10:03:16 +0200

    removed random part from id

--- src/main/java/it/gov/pagopa/receipt/pdf/datastore/utils/BizEventToReceiptUtils.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/datastore/utils/BizEventToReceiptUtils.java
@@ -64,7 +64,7 @@ public class BizEventToReceiptUtils {
         Receipt receipt = new Receipt();
 
         // Insert biz-event data into receipt
-        receipt.setId(bizEvent.getId() + UUID.randomUUID());
+        receipt.setId(bizEvent.getId());
         receipt.setEventId(bizEvent.getId());
 
         EventData eventData = new EventData();
